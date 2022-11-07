5c9bb1c3c7c7ace2158370a96250d427a45b5340
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-11-07 16:58:26 +0200

    task write full name resolve

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const fs = require('fs').promises;
 const promise = fs.readFile('./userData.json', 'utf-8')
 .then(promiseValue=>{
     const obj = JSON.parse(promiseValue);
-    console.log(obj);
+    fs.writeFile('./text.txt', `Hello ${obj.firstName} ${obj.lastName}`);
 })
