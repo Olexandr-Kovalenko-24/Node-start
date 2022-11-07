0c6c7028137dec489754307decc86c0c4184d7bd
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-11-07 15:45:00 +0200

    module.exports/require

---
+++ MyMath.js
@@ -0,0 +1,11 @@
+class MyMath {
+    static sum = (a,b)=> a+b;
+
+    static sub = (a,b)=> a-b;
+
+    static div = (a,b)=> a/b;
+
+    static mul = (a,b)=> a*b;
+}
+
+module.exports = MyMath;
--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-const sum = (...rest) => rest.reduce((acc,el)=>acc+el, 0);
-console.log(sum(3,4,5,2));
+// const sum = (...rest) => rest.reduce((acc,el)=>acc+el, 0);
+// console.log(sum(3,4,5,2));
+
+const mymath = require('./MyMath');
+console.log(mymath.sum(2,6));
