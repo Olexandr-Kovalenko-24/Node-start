a6d98a0ed2e6aa2aefab14c5962cf65d1aa815a8
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-12-16 16:34:02 +0200

    status return

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-// const sum = require('./utils/math');
-
-// console.log('hy')
+const fs = require('fs/promises')
 
 const http = require('http');
 
 const requestHandler = (request, response) => {
+    console.log(request.method)
     if (request.method === 'GET'){
-        response.end('you send GET method')
+        if (request.url === '/') {
+            fs.readFile('./views/index.html', 'utf-8')
+            .then(data=>response.end(data))
+            .catch(error=>response.end(error))
+        } else if (request.url === '/home.html', 'utf-8') {
+            fs.readFile('./views/home.html')
+            .then(data=>response.end(data))
+            .catch(error=>response.end(error))
+        } else if (request.url === '/style.css') {
+            fs.readFile('./views/style.css', 'utf-8')
+            .then(data=>response.end(data))
+            .catch(error=>response.end(error))
+        } else {
+            response.statusCode = 404;
+            return response.end('Not found')
+        }
+
     } else if (request.method === 'POST') {
-        response.end('you send POST method')
-    }
+        response.statusCode = 403;
+        return response.end('Forbidden')
+    } else {        
+        response.statusCode = 403;}
 }
 
 const server = http.createServer(requestHandler);
