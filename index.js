8d344f3e230f5b5cdcc4293ea2cfcb1e893e26a1
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-12-16 17:01:42 +0200

    createand get users

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs/promises')
 
 const http = require('http');
 
+const userDataBase = [];
+
 const requestHandler = (request, response) => {
     console.log(request.method)
     if (request.method === 'GET'){
@@ -9,22 +11,31 @@ const requestHandler = (request, response) => {
             fs.readFile('./views/index.html', 'utf-8')
             .then(data=>response.end(data))
             .catch(error=>response.end(error))
-        } else if (request.url === '/home.html', 'utf-8') {
-            fs.readFile('./views/home.html')
-            .then(data=>response.end(data))
-            .catch(error=>response.end(error))
-        } else if (request.url === '/style.css') {
+        }  else if (request.url === '/style.css') {
             fs.readFile('./views/style.css', 'utf-8')
             .then(data=>response.end(data))
             .catch(error=>response.end(error))
+        } else if (request.url === '/users') {
+            return response.end(JSON.stringify(userDataBase))
         } else {
             response.statusCode = 404;
             return response.end('Not found')
         }
 
     } else if (request.method === 'POST') {
-        response.statusCode = 403;
-        return response.end('Forbidden')
+        if (request.url === '/createuser') {
+            let jsonString = ''
+            request.on('data', (chunk)=>{
+                jsonString += chunk;
+            });
+            request.on('end', () => {
+                const obj = JSON.parse(jsonString);
+                userDataBase.push({...obj,
+                id: userDataBase.length});
+                response.statusCode = 201;
+                return response.end(JSON.stringify(obj));
+            })
+        }
     } else {        
         response.statusCode = 403;}
 }
@@ -34,3 +45,9 @@ const server = http.createServer(requestHandler);
 server.listen(3000, ()=>{
     console.log('app is started')
 });
+
+// else if (request.url === '/home.html', 'utf-8') {
+//     fs.readFile('./views/home.html')
+//     .then(data=>response.end(data))
+//     .catch(error=>response.end(error))
+// }
